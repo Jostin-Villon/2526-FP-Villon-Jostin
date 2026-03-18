44ecc0826dbba4cd0df8b295e3f7cc3a29ff149c
Jostin villon Moran <ja.villonm@uea.edu.ec>
2026-03-17 22:12:47 -0500

    Tarea semana #14

--- semana #14/Tarea semana #14.py
+++ semana #14/Tarea semana #14.py
@@ -1,6 +1,7 @@
 def calcular_promedio(nota1, nota2, nota3):
     promedio = (nota1 + nota2 + nota3) / 3
     return promedio
+print("Calculadora de Promedio de Notas")
 
 n1 = float(input("Ingrese la primera nota: "))
 n2 = float(input("Ingrese la segunda nota: "))
